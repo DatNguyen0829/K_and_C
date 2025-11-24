4094e31d7c1a2507cee0b721a77c76df0a5a9539
DanNguyen <bidat2007@gmail.com>
2025-11-24 11:53:45 -0800

    new edit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <Arduino.h>
 #include "HX711.h"
 
+const uint8_t SCK_PIN = 3;
+const uint8_t DATAOUT_PIN = 2;
+
+HX711 scale;
+
 void setup() {
+    Serial.begin(38400);
+    Serial.println("Setting up the scale");
+
+    //Scale initialized 
+    scale.begin(DATAOUT_PIN, SCK_PIN);
 
 
 
